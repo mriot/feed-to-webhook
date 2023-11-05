68f7177531afdeebf9be142fd34b44d165ab864f
Markus <kremer.markus1996@gmail.com>
2023-11-05 16:58:11 +0100

    link authors and point out if retweet

--- main.py
+++ main.py
@@ -25,7 +25,16 @@ def main():
         if (response.status_code == 200):
             root = ET.fromstring(response.content.decode("utf-8"))
             items = root.findall("channel/item")
+            post_url = items[0].find("link").text
+            is_retweet = feed_owner.find(post_author) == -1
 
+            feed_owner = root.find("channel/title").text # username / @username
+            feed_owner_accountname = feed_owner.split(" / ")[-1] # @username
+            feed_owner_link = "https://twitter.com/" + feed_owner_accountname.replace("@", "")
+
+            post_author = items[0].find("dc:creator", {"dc": "http://purl.org/dc/elements/1.1/"}).text # @username
+            post_author_link = "https://twitter.com/" + post_author.replace("@", "")
+            
             last_item_date = feed.get("last_item_date")
             if last_item_date and last_item_date == items[0].find("pubDate").text and not config["debug"]["force_post"]:
                 continue
@@ -35,12 +44,15 @@ def main():
             with open(config_path, "w") as f:
                 json.dump(config, f, indent=2)
 
-            link = items[0].find("link").text
-
             if feed.get("is_twitter_feed"):
-                link = fx_twitter_url(link)
+                post_url = fx_twitter_url(post_url)
 
-            requests.post(feed["webhook"], {"content": link})
+            if is_retweet:
+                output = f"♻️ [{feed_owner_accountname}]({feed_owner_link}) retweeted [{post_author}]({post_author_link})\n{post_url}"
+            else:
+                output = f"📢 [{feed_owner_accountname}]({feed_owner_link}) tweeted \n{post_url}"
+
+            requests.post(feed["webhook"], {"content": output})
         else:
             requests.post(config["debug"]["webhook"], {
                 "content": f"Error {str(response.status_code)} while fetching feed {feed['url']}"
