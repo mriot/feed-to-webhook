6031a8482fa2f16a08982f193e46c1576d7f1e56
Markus <kremer.markus1996@gmail.com>
2023-11-11 18:15:50 +0100

    fix output

--- bot/rss_feed.py
+++ bot/rss_feed.py
@@ -40,52 +40,7 @@ def rss_feed(feed):
 
         output = discord_embed(post_title, post_url, post_description, enclosure)
 
-        output: {
-            "username": "Webhook",
-            "avatar_url": "https://i.imgur.com/4M34hi2.png",
-            "content": "Text message. Up to 2000 characters.",
-            "embeds": [
-                {
-                    "author": {
-                        "name": "Birdie♫",
-                        "url": "https://www.reddit.com/r/cats/",
-                        "icon_url": "https://i.imgur.com/R66g1Pe.jpg"
-                    },
-                    "title": "Title",
-                    "url": "https://google.com/",
-                    "description": "Text message. You can use Markdown here. *Italic* **bold** __underline__ ~~strikeout~~ [hyperlink](https://google.com) `code`",
-                    "color": 15258703,
-                    "fields": [
-                        {
-                            "name": "Text",
-                            "value": "More text",
-                            "inline": True
-                        },
-                        {
-                            "name": "Even more text",
-                            "value": "Yup",
-                            "inline": True
-                        },
-                        {
-                            "name": "Thanks!",
-                            "value": "You're welcome :wink:"
-                        }
-                    ],
-                    "thumbnail": {
-                        "url": "https://upload.wikimedia.org/wikipedia/commons/3/38/4-Nature-Wallpapers-2014-1_ukaavUI.jpg"
-                    },
-                    "image": {
-                        "url": "https://upload.wikimedia.org/wikipedia/commons/5/5a/A_picture_from_China_every_day_108.jpg"
-                    },
-                    "footer": {
-                        "text": "Woah! So cool! :smirk:",
-                        "icon_url": "https://i.imgur.com/fKL31aD.jpg"
-                    }
-                }
-            ]
-        }
-
         for webhook in feed["webhooks"]:
-            requests.post(webhook, output)
+            requests.post(webhook, json={"embeds": output})
 
     return {"last_item_date": items[0].find("pubDate").text}  # we assume that the first item is the newest one
