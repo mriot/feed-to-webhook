88c421d76500ee648f7952ce7015cccd773dedc8
Markus <kremer.markus1996@gmail.com>
2023-11-12 17:25:37 +0100

    Update discord_embed.py

--- bot/discord_embed.py
+++ bot/discord_embed.py
@@ -1,14 +1,18 @@
-def discord_embed(post_title, post_url, post_description, enclosure):
+def discord_embed(data):
     return [
         {
-            "title": post_title,
-            "url": post_url,
-            "description": post_description,
-            "image": {
-                "url": enclosure
+            "author": {
+                "name": data["feed_owner"],
+                "url": data["feed_owner_link"],
+                # "icon_url": "https://i.imgur.com/R66g1Pe.jpg"
             },
-            "footer": {
-                "text": "Posted at 2021-01-01 00:00:00"
-            }
+            "title": data["post_title"],
+            "url": data.get("post_url", ""),
+            "description": data["post_description"],
+            "color": data["color"],
+            "image": {
+                "url": data["enclosure"]
+            },
+            "timestamp": data.get('post_date', ''),
         }
     ]
