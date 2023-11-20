33451ca019fd69be93ec197ee753f4156d9188aa
Markus <kremer.markus1996@gmail.com>
2023-11-20 16:55:32 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -20,7 +20,8 @@ def main():
             if not timestamps.is_newer(feed):
                 continue
             Sender(feed).send()
-            # timestamps.update(url, feed.currentTimestamp)
+            print(feed.latest_timestamp)
+            timestamps.update(url, feed.latest_timestamp)
         except Exception as e:
             requests.post(config["error_webhook"], {"content": f"Error {str(e)} while fetching twitter feed {tfeed['url']}"})
 
