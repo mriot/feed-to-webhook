c0f416bae389c4d391c8319aedd1d38c916b2ebe
Markus <kremer.markus1996@gmail.com>
2023-11-27 00:10:36 +0100

    remove obsolete timestamps from file

--- src/timestamps.py
+++ src/timestamps.py
@@ -8,6 +8,7 @@ class Timestamps:
     def __init__(self):
         self._timestamps_file = YamlFile("feed_timestamps.yaml")
         self._timestamps = self._timestamps_file.read() or {}
+        self._new_timestamps = {}
 
     def get(self, url):
         timestamp = self._timestamps.get(url)
@@ -17,10 +18,10 @@ class Timestamps:
         return parse(timestamp)
 
     def update(self, feed):
-        self._timestamps[feed.url] = feed.latest_timestamp
+        self._new_timestamps[feed.url] = feed.latest_timestamp
 
     def write(self):
-        self._timestamps_file.write(self._timestamps)
+        self._timestamps_file.write(self._new_timestamps)
 
     def filter_out_old_posts(self, feed):
         feed.feed_items = [post for post in feed.feed_items if post.get_pubdate() > self.get(feed.url)]
