7c1272506eb182bb25720345aa14cedb78eaecc1
Markus <kremer.markus1996@gmail.com>
2023-11-27 20:49:51 +0100

    rename config to CONFIG

--- src/main.py
+++ src/main.py
@@ -7,34 +7,41 @@ from file_handler import YamlFile
 
 
 def main():
-    config = YamlFile("config.yaml", False).read()
+    CONFIG = YamlFile("config.yaml", False).read()
     timestamps = Timestamps()
 
     # twitter
-    for tfeed in config.get("twitter_feeds", []):
+    for tfeed in CONFIG.get("twitter_feeds", []):
         try:
-            url, webhooks, include_retweets = tfeed.get("url"), tfeed.get("webhooks"), tfeed.get("include_retweets")
-            feed = TwitterFeed(url, webhooks, include_retweets)
+            feed = TwitterFeed(
+                tfeed.get("url"),
+                tfeed.get("webhooks"),
+                tfeed.get("include_retweets")
+            )
             feed.load()
             timestamps.filter_out_old_posts(feed)
             Sender(feed).send()
             timestamps.update(feed)
         except Exception as e:
             print(e)
-            requests.post(config["error_webhook"], {"content": f"Error {str(e)} while fetching twitter feed {tfeed['url']}"})
+            requests.post(CONFIG.get("error_webhook"), {"content": f"Error {str(e)} while fetching twitter feed {tfeed['url']}"})
 
     # rss
-    for rfeed in config.get("rss_feeds", []):
+    for rfeed in CONFIG.get("rss_feeds", []):
         try:
-            url, webhooks, summarize = rfeed.get("url"), rfeed.get("webhooks"), rfeed.get("summarize")
-            feed = RssFeed(url, webhooks, summarize)
+            feed = RssFeed(
+                rfeed.get("url"),
+                rfeed.get("webhooks"),
+                embed_color=rfeed.get("embed_color"),
+                summarize=rfeed.get("summarize")
+            )
             feed.load()
             timestamps.filter_out_old_posts(feed)
             Sender(feed).send_json()
             timestamps.update(feed)
         except Exception as e:
             print(e)
-            requests.post(config["error_webhook"], {"content": f"Error {str(e)} while fetching RSS feed {rfeed['url']}"})
+            requests.post(CONFIG.get("error_webhook"), {"content": f"Error {str(e)} while fetching RSS feed {rfeed['url']}"})
 
     timestamps.write()
 
