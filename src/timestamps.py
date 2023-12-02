d64722eebb841911af686f81a1fa7f0f7111d632
Markus <kremer.markus1996@gmail.com>
2023-12-02 16:14:14 +0100

    dont remove old/not updated timestamps

--- src/timestamps.py
+++ src/timestamps.py
@@ -8,7 +8,6 @@ class Timestamps:
     def __init__(self):
         self._timestamps_file = YamlFile("feed_timestamps.yaml")
         self._timestamps = self._timestamps_file.read() or {}
-        self._new_timestamps = {}
 
     def get(self, url):
         timestamp = self._timestamps.get(url)
@@ -18,10 +17,10 @@ class Timestamps:
         return parse(timestamp)
 
     def update(self, feed):
-        self._new_timestamps[feed.url] = feed.latest_timestamp
+        self._timestamps[feed.url] = feed.latest_timestamp
 
     def write(self):
-        self._timestamps_file.write(self._new_timestamps)
+        self._timestamps_file.write(self._timestamps)
 
     def filter_out_old_posts(self, feed):
         feed.feed_items = [post for post in feed.feed_items if post.get_pubdate() > self.get(feed.url)]
