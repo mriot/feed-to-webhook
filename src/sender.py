530ade21dceccec2b8ed7b609d36137750f94497
Markus <kremer.markus1996@gmail.com>
2023-12-02 16:06:12 +0100

    print status if webhook returns non 200 code

--- src/sender.py
+++ src/sender.py
@@ -13,11 +13,13 @@ class Sender:
         for post in posts:
             for webhook in self.feed.webhooks:
                 res = requests.post(webhook, {"content": post})
-                print(f"Status Code: {res.status_code}, Message: {res.reason}")
+                if res.status_code < 200 or res.status_code > 299:
+                    print(f"Webhook '{webhook}' responded with status code {res.status_code} and message '{res.reason}'")
 
     def send_json(self):
         posts = self.feed.prepare_content().final_items_to_be_posted
         for post in posts:
             for webhook in self.feed.webhooks:
                 res = requests.post(webhook, json={"embeds": post}, headers={"Content-Type": "application/json"})
-                print(f"Status Code: {res.status_code}, Message: {res.reason}")
+                if res.status_code < 200 or res.status_code > 299:
+                    print(f"Webhook '{webhook}' responded with status code {res.status_code} and message '{res.reason}'")
