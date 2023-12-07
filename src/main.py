7f8135583c2866fb62dcdb2ab6149d929a65769d
Markus <kremer.markus1996@gmail.com>
2023-12-07 01:58:39 +0100

    Refactor sender functionality

--- src/main.py
+++ src/main.py
@@ -13,51 +13,18 @@ def main():
 
     CONFIG = YamlFile("config.yaml", False).read()
     timestamps = Timestamps()
+    sender = Sender()
 
-    # new_items = []
-
-    # TODO sender function
-    # - gets list of feed objects (embeds are already prepared and stored in feed object)
-    # - extract embeds from all feeds and sort them by timestamp (store them in a list)
-    # - update timestamps (or do it in main() ?)
-    # - send embeds to webhooks (add rate limit handling)
-
-    # twitter
-    for tfeed in CONFIG.get("twitter_feeds", []):
+    def process_feed(feed):
         try:
-            feed = TwitterFeed(
-                tfeed.get("url"),
-                tfeed.get("webhooks"),
-                tfeed.get("embed_color"),
-                tfeed.get("exclude_retweets"),
-                tfeed.get("override_domain"),
-            )
-            feed.load()  # TODO automatically load feed upon creation
-            feed.remove_old_posts(timestamps)  # REPLACED: timestamps.filter_out_old_posts(feed)
-            # TODO we dont need to continue if there are no new posts
-            feed.prepare_content()  # TODO rename to make_embeds()
-            # TODO store feed object in a list
-            # new_items.extend(feed.final_items_to_be_posted) # we should not need this anymore
-            # Sender(feed).send_json() # TODO make function, drop class
-            # timestamps.update(feed) # will be handled by sender function
-        except Exception as e:
-            tb = traceback.TracebackException.from_exception(e).stack[-1]
-            err = f"❌ {e}\n-> Error occurred in file '{tb.filename}' at line {tb.lineno} in function '{tb.name}'"
-            print(err)
-            requests.post(CONFIG.get("error_webhook"), {"content": err})
-
-    # rss
-    for rfeed in CONFIG.get("rss_feeds", []):
-        try:
-            feed = RssFeed(
-                rfeed.get("url"),
-                rfeed.get("webhooks"),
-                rfeed.get("embed_color"),
-                rfeed.get("summarize")  # TODO: implement
-            )
             feed.load()
-            timestamps.filter_out_old_posts(feed)
-            Sender(feed).send_json()
+            feed.remove_old_posts(timestamps)
+
+            if not feed.feed_items:
+                return
+
+            feed.make_embeds()
+            sender.add(feed)
             timestamps.update(feed)
         except Exception as e:
             tb = traceback.TracebackException.from_exception(e).stack[-1]
@@ -65,12 +32,28 @@ def main():
             print(err)
             requests.post(CONFIG.get("error_webhook"), {"content": err})
 
-    # timestamps.write()
+    # twitter
+    for tfeed in CONFIG.get("twitter_feeds", []):
+        process_feed(TwitterFeed(
+            tfeed.get("url"),
+            tfeed.get("webhooks"),
+            tfeed.get("embed_color"),
+            tfeed.get("exclude_retweets"),
+            tfeed.get("override_domain"),
+        ))
 
-    # print(new_items)
-    # print(new_items[0][0].get("title"))
-    # new_items.sort(key=lambda x: x[0]["timestamp"])
-    # print(new_items)
+    # rss
+    for rfeed in CONFIG.get("rss_feeds", []):
+        process_feed(RssFeed(
+            rfeed.get("url"),
+            rfeed.get("webhooks"),
+            rfeed.get("embed_color"),
+            rfeed.get("summarize")  # TODO: implement
+        ))
+
+    sender.send_embeds()
+
+    # timestamps.write()
 
     END_TIME = time.time()
     print(f"Execution time: {round(END_TIME - START_TIME, 1)} seconds")
--- src/sender.py
+++ src/sender.py
@@ -3,11 +3,42 @@ from feed import Feed
 
 
 class Sender:
-    def __init__(self, feed: Feed):
-        if not isinstance(feed, Feed):
-            raise TypeError("'feed' must be an instance of Feed")
-        self.feed = feed
+    def __init__(self):
+        self.feeds = []
 
+    def add(self, feed: Feed):
+        self.feeds.append(feed)
+
+    def sort_embeds(self):
+        # embeds = []
+        # for feed in self.feeds:
+        #     embeds.extend(feed.final_items_to_be_posted)
+        # embeds.sort(key=lambda x: x[0]['timestamp'])
+        # return embeds
+        embeds = []
+        for feed in self.feeds:
+            embeds.extend({"feed": feed, "embeds": feed.final_items_to_be_posted})
+        embeds.sort(key=lambda x: x["feed"]['latest_timestamp'])
+        return embeds
+
+    def send_embeds(self):
+        for embed in self.sort_embeds():
+            for webhook in embed.feed.webhooks:
+                res = requests.post(webhook, json={"embeds": embed.embeds}, headers={"Content-Type": "application/json"})
+
+                # TODO - handle rate limit
+                if res.status_code == 429:
+                    print(f"Rate limit exceeded for webhook {webhook}")
+                    retry_after = res.headers.get('retry_after')
+                    if retry_after:
+                        # time.sleep(int(retry_after))
+                        # threading.Timer(int(retry_after), send_request, args=[data]).start()
+                        pass
+
+                if res.status_code > 299:
+                    raise Exception(f"**Status code {res.status_code}** ({res.reason})\n**Feed**: {embed.feed.url}\n**Webhook: **{webhook}\n**Payload**:\n```json\n{res.request.body}```")
+
+    # DEPRECATED
     def send(self):
         posts = self.feed.prepare_content().final_items_to_be_posted
         for post in posts:
@@ -16,6 +47,7 @@ class Sender:
                 if res.status_code > 299:
                     raise Exception(f"Webhook '{webhook}' responded with status code {res.status_code} and message '{res.reason}'")
 
+     # DEPRECATED
     def send_json(self):
         posts = self.feed.prepare_content().final_items_to_be_posted
         for post in posts:
