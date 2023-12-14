480a0b7506d34145118f3fee333d37b773492216
Markus <kremer.markus1996@gmail.com>
2023-12-14 17:24:01 +0100

    improve error message

--- src/feed.py
+++ src/feed.py
@@ -34,7 +34,7 @@ class Feed(ABC):
         self.feed_data_dict = feedparser.parse(self.url)
 
         if self.feed_data_dict.get("bozo_exception"):
-            raise Exception(f"Malformed feed data {self.feed_data_dict.get('bozo_exception')}")
+            raise Exception(f"Malformed feed data for URL {self.url}\n'{self.feed_data_dict.get('bozo_exception')}'")
 
         self.latest_timestamp = self.feed_data_dict.get("items", [])[0].get("published")
         self._make_feed_items()
