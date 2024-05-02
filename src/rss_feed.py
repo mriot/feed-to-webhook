a390205e142e882c428689daab63aa025b299f6c
Markus <kremer.markus1996@gmail.com>
2024-05-02 18:28:22 +0200

    refactor + fix issue with links

--- src/rss_feed.py
+++ src/rss_feed.py
@@ -1,6 +1,7 @@
 from feed import Feed
-from html2text import html2text
-from bs4 import BeautifulSoup
+from html2text import html2text as html2md
+from bs4 import BeautifulSoup as HtmlParser
+from bs4.element import Tag
 
 
 class RssFeed(Feed):
@@ -9,33 +10,40 @@ class RssFeed(Feed):
         self.embed_color = int(embed_color if embed_color is not None else "738adb", 16)
 
     def make_embeds(self):
-        d = self.feed_data_dict
-        avatar = self.feed_data_dict.feed.get("image", {}).get("url")
+        for item in reversed(self.feed_items[:5]):  # max items posted at once
+            html = HtmlParser(item.description, "html.parser")
 
-        for item in self.feed_items[:5]:
-            soup = BeautifulSoup(item.item_root.get("description"), "html.parser")
+            # extract first image src from description (if any) - we pass it later as part of embed
+            img_tag = html.find("img")
+            img_src = img_tag.get("src") if isinstance(img_tag, Tag) else ""
 
-            # extract first image src from description - we pass it later as part of embed
-            img_tag = soup.find("img")
-            img_src = img_tag["src"] if img_tag else ""
+            # remove "http://" and "https://" from the link text
+            # (discord does not support markdown links if their name contains http:// or https://)
+            for link in html.find_all("a"):
+                if link.text.find("http://") or link.text.find("https://"):
+                    link.string = link.text.replace("http://", "").replace(
+                        "https://", ""
+                    )
 
             # remove certain html tags from description before transforming to markdown
             tags_to_remove = ["img"]
             for tag_name in tags_to_remove:
-                for tag in soup.find_all(tag_name):
+                for tag in html.find_all(tag_name):
                     tag.decompose()
 
             embed = [
                 {
-                    "type": "image",  # required for images without extension ¯\_(ツ)_/¯
+                    "type": "image",
                     "color": self.embed_color,
-                    "author": {"name": d.feed.get("title"), "url": d.feed.get("link")},
-                    "thumbnail": {"url": avatar},
-                    "title": item.item_root.get("title"),
-                    "url": item.item_root.get("link"),
-                    "description": html2text(str(soup)),
-                    "image": {"url": img_src or item.item_root.get("enclosure")},
-                    "timestamp": str(item.get_pubdate()),
+                    # ----------------------------------
+                    "thumbnail": {"url": self.avatar_url},
+                    "author": {"name": self.title, "url": self.link},
+                    # ----------------------------------
+                    "title": item.title,
+                    "url": item.link,
+                    "description": html2md(str(html), bodywidth=0),
+                    "image": {"url": img_src or ""},
+                    "timestamp": str(item.pub_date),
                 }
             ]
 
