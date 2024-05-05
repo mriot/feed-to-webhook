6f94c363af26d008536debe0277713b4951893de
Markus <kremer.markus1996@gmail.com>
2024-05-05 17:28:39 +0200

    add encoding

--- src/file_handler.py
+++ src/file_handler.py
@@ -8,12 +8,12 @@ class JsonFile:
         self.file_path = path.join(path.dirname(path.realpath(__file__)), file_name)
 
         if create and not path.exists(self.file_path):
-            with open(self.file_path, "w") as file:
+            with open(self.file_path, "w", encoding="utf-8") as file:
                 json.dump({}, file, ensure_ascii=False, indent=4)
 
     def read(self):
         try:
-            with open(self.file_path, "r") as file:
+            with open(self.file_path, "r", encoding="utf-8") as file:
                 return json.load(file) or {}
         except json.JSONDecodeError as e:
             print(f"Error parsing JSON file '{self.file_path}': {e}")
@@ -23,5 +23,5 @@ class JsonFile:
             sys.exit(1)
 
     def write(self, data):
-        with open(self.file_path, "w") as file:
+        with open(self.file_path, "w", encoding="utf-8") as file:
             json.dump(data, file, ensure_ascii=False, indent=4)
