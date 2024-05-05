96a4b059e8f806c469e4cf68d4fbb39c268537e2
Markus <kremer.markus1996@gmail.com>
2024-05-05 16:57:07 +0200

    add types; improve error handling

--- src/feed.py
+++ src/feed.py
@@ -1,48 +1,55 @@
+from datetime import datetime
+from typing import Optional
 import dateutil.parser
 import feedparser
 
 
 class Feed:
-    def __init__(self, url, webhooks, embed_color=None):
-        self.url = url
-        self.webhooks = webhooks
-        self.embed_color = int(embed_color if embed_color is not None else "738adb", 16)
+    def __init__(self, url: str, webhooks: list[str], embed_color: Optional[str] = None):
+        self.url: str = url
+        self.webhooks: list[str] = webhooks
+        self.embed_color: int = int(embed_color if embed_color is not None else "738adb", 16)
 
         # download and parse feed
-        feed_data = feedparser.parse(self.url)
+        feed_data: feedparser.FeedParserDict = feedparser.parse(self.url)
 
         if feed_data.get("bozo_exception"):
-            raise Exception(
+            raise ValueError(
                 f"Failed to parse feed from URL {self.url}\n{feed_data.get('bozo_exception')}"
             )
 
-        channel = feed_data.get("feed")  # information about the feed itself
-        entries = feed_data.get("entries")  # data about the actual posts
+        channel = feed_data.get("feed")
+        entries = feed_data.get("entries")
+
+        if not isinstance(channel, dict):
+            raise TypeError(f"Failed to extract feed data from {self.url}")
+
+        if not isinstance(entries, list):
+            raise TypeError(f"Failed to extract posts from feed {self.url}")
 
         if not entries:
-            raise Exception(f"Failed to extract feed items from {self.url}")
+            raise ValueError(f"No posts found in feed {self.url}")
 
-        self.feed_title = channel.get("title", "Untitled")
-        self.feed_description = channel.get("description", "")
-        self.feed_link = channel.get("link", "")
-        self.feed_avatar_url = channel.get("image", {}).get("url", "")
-        self.feed_gernerator = channel.get("generator", "")
+        self.feed_title: str = channel.get("title", "Untitled")
+        self.feed_description: str = channel.get("description", "")
+        self.feed_link: str = channel.get("link", "")
+        self.feed_avatar_url: str = channel.get("image", {}).get("url", "")
 
-        self.posts = [Post(item) for item in entries[:25]]  # upper limit just in case
-        self.latest_timestamp = max((item.post_pub_date) for item in self.posts)
+        self.posts: list[Post] = [Post(item) for item in entries[:25]]  # upper limit just in case
+        self.latest_timestamp: datetime = max((item.post_pub_date) for item in self.posts)
 
-        self.new_embedded_posts = []  # populated by make_embeds()
+        self.new_embedded_posts: list = []  # populated by make_embeds()
 
-    def remove_old_posts(self, timestamp):
-        self.posts = [post for post in self.posts if post.post_pub_date > timestamp]
+    def remove_old_posts(self, timestamp: datetime) -> None:
+        self.posts: list[Post] = [post for post in self.posts if post.post_pub_date > timestamp]
 
 
 class Post:
     def __init__(self, item):
-        self.post_link = item.get("link", "")
-        self.post_title = item.get("title", "")
-        self.post_pub_date = dateutil.parser.parse(item.get("published"))
-        self.post_author = item.get("author", "")
-        self.post_description = item.get("description", "")
-        self.post_media = item.get("media_content", [])
-        self.post_content = item.get("content", [])
+        self.post_link: str = item.get("link", "")
+        self.post_title: str = item.get("title", "")
+        self.post_pub_date: datetime = dateutil.parser.parse(item.get("published"))
+        self.post_author: str = item.get("author", "")
+        self.post_description: str = item.get("description", "")
+        self.post_media: list = item.get("media_content", [])
+        self.post_content: list = item.get("content", [])
