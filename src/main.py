68beba8e6aad258a0092458333e4823e6ed65d0a
Markus <kremer.markus1996@gmail.com>
2024-05-05 17:26:19 +0200

    improve error handling

--- src/main.py
+++ src/main.py
@@ -1,13 +1,10 @@
-import json
-import requests
-from sender import Sender
-from rss_feed import RssFeed
-from timestamps import Timestamps
-from file_handler import JsonFile
-import traceback
 import time
 
-from utils import FeedConfigError
+from file_handler import JsonFile
+from rss_feed import RssFeed
+from sender import Sender
+from timestamps import Timestamps
+from utils import FeedConfigError, handle_error_reporting
 
 
 def main():
@@ -24,37 +21,43 @@ def main():
         raise KeyError("Could not find the mandatory key 'feeds' in the config file.")
 
     for feed_config in feeds:
-        # feed config validation
-        if missing_keys := [key for key in ["url", "webhooks"] if key not in feed_config]:
-            raise FeedConfigError(f"{' and '.join(missing_keys)} not found", feed_config)
+        try:
+            # -- start of config validation --
+            if missing_keys := [key for key in ["url", "webhooks"] if key not in feed_config]:
+                raise FeedConfigError(f"{' and '.join(missing_keys)} not found", feed_config)
 
-        if url := feed_config.get("url"):
-            if not isinstance(url, str):
-                raise FeedConfigError("Feed URL must be a string", feed_config)
-        else:
-            raise FeedConfigError("No URL configured", feed_config)
+            if url := feed_config.get("url"):
+                if not isinstance(url, str):
+                    raise FeedConfigError("Feed URL must be a string", feed_config)
+            else:
+                raise FeedConfigError("No URL configured", feed_config)
 
-        if webhooks := feed_config.get("webhooks"):
-            if not isinstance(webhooks, list):
-                raise FeedConfigError("Webhooks must be a list of URLs.", feed_config)
-        else:
-            raise FeedConfigError("No webhooks configured", feed_config)
+            if webhooks := feed_config.get("webhooks"):
+                if not isinstance(webhooks, list):
+                    raise FeedConfigError("Webhooks must be a list of URLs.", feed_config)
+            else:
+                raise FeedConfigError("No webhooks configured", feed_config)
 
-        feed = RssFeed(url, webhooks, feed_config.get("embed_color"))
+            # -- end of config validation --
 
-        # removes posts that are older than the last time we checked
-        # feed.remove_old_posts(timestamps.get(feed.url))
-        feed.remove_old_posts(timestamps.get(feed.url))
+            feed = RssFeed(url, webhooks, feed_config.get("embed_color"))
 
-        # always update - also ensures that new feeds are added to the timestamps file
-        timestamps.update(feed.url, feed.latest_timestamp)
+            # removes posts that are older than the last time we checked
+            feed.remove_old_posts(timestamps.get(feed.url))
 
-        # skip if no new posts
-        if not feed.posts:
-            continue
+            # always update - also ensures that new feeds are added to the timestamps file
+            timestamps.update(feed.url, feed.latest_timestamp)
 
-        feed.make_embeds()
-        sender.add(feed)
+            # skip feed if no new posts
+            if not feed.posts:
+                continue
+
+            feed.make_embeds()
+            sender.add(feed)
+
+        # TODO - add more specific error handling
+        except Exception as feed_err:
+            handle_error_reporting(feed_err)
 
     sender.send_embeds()
 
@@ -68,26 +71,4 @@ if __name__ == "__main__":
     try:
         main()
     except Exception as err:
-        tb = traceback.TracebackException.from_exception(err).stack[-1]
-        err_msg = f"Error raised in '{tb.filename}' at line {tb.lineno} in function '{tb.name}'"
-
-        if isinstance(err, FeedConfigError):
-            err_msg = f"```{json.dumps(err.feed_config, indent=2)}``` \n {err_msg}"
-
-        print(f"ERROR: {err}\n{err_msg}")
-
-        # also send the error to the error webhook if it is configured
-        if errhook := JsonFile("config.json", False).read().get("error_webhook"):
-            requests.post(
-                errhook,
-                json={
-                    "embeds": [
-                        {
-                            "title": f"ERROR:  {err}",
-                            "description": err_msg,
-                            "color": 16711680,
-                        }
-                    ]
-                },
-                headers={"Content-Type": "application/json"},
-            )
+        handle_error_reporting(err)
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,10 @@
+import json
 import re
+import traceback
+
+import requests
+
+from file_handler import JsonFile
 
 
 class FeedConfigError(Exception):
@@ -12,3 +18,33 @@ class FeedConfigError(Exception):
 def strip_protocol(string: str) -> str:
     """Removes all URL protocols from a string."""
     return re.sub(r"^[a-zA-Z]+://", "", string)
+
+
+def handle_error_reporting(err: Exception) -> None:
+    """Handles error reporting and formatting."""
+    tb = traceback.TracebackException.from_exception(err).stack[-1]
+    err_msg = f"Error raised in '{tb.filename}' at line {tb.lineno} in function '{tb.name}'"
+
+    if isinstance(err, FeedConfigError):
+        err_msg = f"```{json.dumps(err.feed_config, indent=2)}``` \n {err_msg}"
+
+    print(f"ERROR: {err}\n{err_msg}")
+
+    # also send the error to the error webhook if it is configured
+    if errhook := JsonFile("config.json", False).read().get("error_webhook"):
+        requests.post(
+            errhook,
+            json={
+                "embeds": [
+                    {
+                        "title": f"ERROR:  {err}",
+                        "description": err_msg,
+                        "color": 16711680,
+                    }
+                ]
+            },
+            headers={"Content-Type": "application/json"},
+            timeout=10,
+        )
+    else:
+        print("Warning: No error webhook configured")
