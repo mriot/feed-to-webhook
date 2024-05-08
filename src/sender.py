dc6e4cbadf92965240a1f32069ac123c3d43ec45
Markus <kremer.markus1996@gmail.com>
2024-05-08 15:59:44 +0200

    handle sending-errors without terminating the program

--- src/sender.py
+++ src/sender.py
@@ -4,7 +4,7 @@ import time
 import requests
 
 from feed import Feed
-from utils import WebhookHTTPError
+from utils import WebhookHTTPError, handle_error_reporting
 
 
 class Sender:
@@ -25,38 +25,46 @@ class Sender:
     def send(self):
         MAX_RETRY_LIMIT = 3
 
-        for sorted_embeds in self.sort_embeds():
-            for webhook in sorted_embeds["feed"].webhooks:
-                for counter in range(MAX_RETRY_LIMIT):
-                    res = requests.post(
-                        webhook,
-                        json={"embeds": sorted_embeds["embed"]},
-                        headers={"Content-Type": "application/json"},
-                        timeout=5,
-                    )
+        for sorted_embed in self.sort_embeds():
+            for webhook in sorted_embed["feed"].webhooks:
+                try:
+                    for counter in range(MAX_RETRY_LIMIT):
+                        res = requests.post(
+                            webhook,
+                            json={"embeds": sorted_embed["embed"]},
+                            headers={"Content-Type": "application/json"},
+                            timeout=5,
+                        )
 
-                    # Rate limit handling
-                    if res.status_code == 429:
-                        print(f"Rate limit reached for webhook {webhook}")
-                        retry_after = min(int(res.headers.get("retry_after", 1)), 5)
-                        print(f"Retrying in {retry_after} seconds. {counter+1}/{MAX_RETRY_LIMIT}")
+                        # Rate limit handling
+                        if res.status_code == 429:
+                            print(f"Rate limit reached for webhook {webhook}")
+                            retry_after = min(int(res.headers.get("retry_after", 1)), 5)
+                            print(
+                                f"Retrying in {retry_after} seconds. {counter+1}/{MAX_RETRY_LIMIT}"
+                            )
 
-                        time.sleep(int(retry_after))
-                        continue
+                            time.sleep(int(retry_after))
+                            continue
 
-                    # Check for any other error codes
-                    if res.status_code >= 300:
+                        # Check for any other error codes
+                        if res.status_code >= 300:
+                            raise WebhookHTTPError(
+                                f"Status code {res.status_code} ({res.reason})",
+                                f"Feed: {sorted_embed['feed'].url}\n"
+                                f"Webhook: {webhook}\nResponse:",
+                                json.dumps(res.json(), indent=2),
+                            )
+
+                        break  # if we reach this point, the request was successful
+                    else:
+                        # we could not make it past the rate limit for some reason
                         raise WebhookHTTPError(
-                            f"Status code {res.status_code} ({res.reason})",
-                            f"Feed: {sorted_embeds['feed'].url}\n" f"Webhook: {webhook}\nResponse:",
+                            f"Rate limit exceeded: {res.status_code} ({res.reason})",
+                            f"Feed: {sorted_embed['feed'].url}\n" f"Webhook: {webhook}",
                             json.dumps(res.json(), indent=2),
                         )
 
-                    break  # if we reach this point, the request was successful
-                else:
-                    # we could not make it past the rate limit for some reason
-                    raise WebhookHTTPError(
-                        f"Rate limit exceeded: {res.status_code} ({res.reason})",
-                        f"Feed: {sorted_embeds['feed'].url}\n" f"Webhook: {webhook}",
-                        json.dumps(res.json(), indent=2),
-                    )
+                except WebhookHTTPError as wh_err:
+                    handle_error_reporting(wh_err)
+                    continue  # try the next webhook
