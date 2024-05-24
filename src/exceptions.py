a4fbc979652378e7aa23026b85740afb53028584
Markus <kremer.markus1996@gmail.com>
2024-05-24 13:08:42 +0200

    dev

--- src/exceptions.py
+++ src/exceptions.py
@@ -4,6 +4,10 @@ import traceback
 from os import path
 from typing import Optional
 
+import requests
+
+from file_handler import JsonFile
+
 
 logging.basicConfig(
     filename=path.join(path.dirname(path.realpath(__file__)), "../ftw.log"),
@@ -20,30 +24,56 @@ class CustomBaseException(Exception):
         self.body = body
         self.attachment = attachment
 
-        self.traceback_info = traceback.extract_stack()[:-2]  # Exclude this + call to extract_stack
-        last_frame = self.traceback_info[-1]
+        # Exclude the calls to extract_stack and this class to get the actual error origin
+        self.tb = traceback.extract_stack()[:-2][-1]
+        self.tb_str = f"Raised in '{self.tb.filename}' on line {self.tb.lineno} in {self.tb.name}"
 
-        self.file_name = last_frame.filename
-        self.line_number = last_frame.lineno
-        self.function_name = last_frame.name
-        self.code_line = last_frame.line
+    def print_to_console(self):
+        print(f"ERROR: {self.title}\n{self.body}\n{self.tb_str}")
 
-    def print(self):
-        pass
-
-    def log(self, level=logging.ERROR):
-        logging.log(
-            level,
-            f"{self.title} : {self.body}  -  {self.file_name} - {self.line_number} - {self.function_name} - {self.code_line}",
-        )
+    def log_to_file(self, level=logging.ERROR):
+        logging.log(level, f"{self.title} - {self.tb_str}\nData: {self.body}")
 
     def send_to_webhook(self):
-        pass
+        if errhook := JsonFile("config.json", False).read().get("error_webhook"):
+            headers = {
+                "Content-Type": "application/json" if not self.attachment else "multipart/form-data"
+            }
+            content = {
+                "embeds": [
+                    {
+                        "title": f"ERROR: {self.title}",
+                        "description": f"{self.body}\n{self.tb_str}",
+                        "color": 16711680,
+                    }
+                ]
+            }
+
+            res = requests.post(
+                errhook,
+                json=content,
+                headers=headers,
+                files=[("file", self.attachment)] if self.attachment else None,
+                timeout=10,
+            )
+
+            if res.status_code >= 300:
+                logging.error(
+                    f"Error webhook returned status code {res.status_code} ({res.reason})"
+                )
+        else:
+            logging.warning("Trying to send error to webhook, but no error webhook is configured.")
 
     def handle(self):
-        self.print()
-        self.log()
+        self.print_to_console()
+        self.log_to_file()
         self.send_to_webhook()
+
+
+class FeedParseError(CustomBaseException):
+    def __init__(self, title, feed_data):
+        self.feed_data = feed_data
+        super().__init__(title, attachment=feed_data)
 
 
 class FeedConfigError(CustomBaseException):
